3ff562270baa076fb87c2bede15ca8ea8f83c10b
Luiz Eduardo <29684170+mnzjupiter@users.noreply.github.com>
2020-06-07 12:37:34 -0300

    Arquivo de testes de código do curso de programação.

---
+++ week1/text.py
@@ -0,0 +1,66 @@
+"""
+varA = input()
+varB = input()
+
+if (type(varA) == str) or (type(varB) == str):
+    print("string involved")
+elif varA > varB:
+    print("bigger")
+elif varA == varB:
+    print("equal")
+elif varA < varB:
+    print("smaller")
+
+"""
+
+"""
+num = 0
+while num <= 10:
+    num += 1
+    if num % 2 == 0:
+        print(num)
+print("Good Bye")
+"""
+
+"""
+print("Hello")
+num = 12
+while num > 1:
+    num -= 1
+    if num % 2 == 0:
+        print(num)
+"""
+
+"""
+end = 6
+n = 1
+soma = 0
+while n <= end:
+    soma = soma + n
+    n += 1
+print(soma)
+"""
+
+"""
+for i in range(2, 12, 2):
+    print(i)
+print("Goodbye!")
+"""
+
+
+"""
+print("Hello!")
+for i in range(10, 1, -2):
+    print(i)
+"""
+
+"""
+end = 20
+n = 1
+z = 0
+for i in range(0, end):
+    z = z + n
+    n += 1
+print(z)
+"""
+
