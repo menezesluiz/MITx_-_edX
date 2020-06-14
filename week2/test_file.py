124c40362c0b1d8f50671e9a52db5600214209ea
Luiz Eduardo <29684170+mnzjupiter@users.noreply.github.com>
2020-06-14 19:22:01 -0300

    Week2
    Week2

--- week2/test_file.py
+++ week2/test_file.py
@@ -82,22 +82,3 @@ if abs(guess ** 2 - x) >= epsilon:
 else:
     print('suceceeded: ' + str(guess))
 """
-
-x = 25
-epsilon = 0.01
-numGuesses = 0
-low = 1.0
-high = x
-ans = (high + low) / 2.0
-
-while abs(ans ** 2 - x) >= epsilon:
-    print("low = " = str(low) + "high = " + str(high) + "ans = " + str(ans))
-    numGuesses += 1
-    if ans ** 2 < x:
-        low = ans
-    else:
-        high = ans
-    ans = (high + low) / 2.0
-
-print("numGuesses = " + str(numGuesses))
-print(str(ans) + " is close to square root  ")
