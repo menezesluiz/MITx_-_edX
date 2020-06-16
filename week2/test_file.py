27e60db5d402f791cb3af3eb580d17545a4d3fde
Luiz Eduardo <29684170+mnzjupiter@users.noreply.github.com>
2020-06-15 23:47:12 -0300

    Week2

--- week2/test_file.py
+++ week2/test_file.py
@@ -99,3 +99,59 @@ def evalQuadratic(a, b, c, x):
     return(a * (x ** 2) + (b * x) + c)
 print(evalQuadratic(a, b, c, x))
 """
+
+
+"""
+x = input()
+y = input()
+z = input()
+
+
+def a(x):
+    '''
+    x: int or float.
+    '''
+    return x + 1
+print(a(a(a(6))))
+
+
+
+def b(x):
+    '''
+    x: int or float.
+    '''
+    return x + 1.0
+
+
+def c(x, y):
+    '''
+    x: int or float.
+    y: int or float.
+    '''
+    return x + y
+
+
+def d(x, y):
+    '''
+    x: Can be of any type.
+    y: Can be of any type.
+    '''
+    return x > y
+
+
+def e(x, y, z):
+    '''
+    x: Can be of any type.
+    y: Can be of any type.
+    z: Can be of any type.
+    '''
+    return x >= y and x <= z
+
+
+def f(x, y):
+    '''
+    x: int or float.
+    y: int or float
+    '''
+    x + y - 2
+"""
