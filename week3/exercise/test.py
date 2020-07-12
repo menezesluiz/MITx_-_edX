ae9c785ac4a5f7ab3f47e1123bfc142dc21e8e18
Luiz Eduardo <29684170+menezesluiz@users.noreply.github.com>
2020-07-12 15:54:34 -0300

    Week3

---
+++ week3/exercise/test.py
@@ -0,0 +1,14 @@
+def applyEachTo(L, x):
+    result = []
+    for i in range(len(L)):
+        result.append(L[i](x))
+    return result
+
+def square(a):
+    return a*a
+
+def halve(a):
+    return a/2
+
+def inc(a):
+    return a+1
