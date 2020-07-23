7ce3cb8791b2003090a127dce4b119884fc8afb1
Luiz Eduardo <29684170+menezesluiz@users.noreply.github.com>
2020-07-23 00:05:28 -0300

    MITx_edX

---
+++ week5/exercise/exercise3.py
@@ -0,0 +1,20 @@
+class Weird(object):
+    def __init__(self, x, y):
+        self.y = y
+        self.x = x
+    def getX(self):
+        return x
+    def getY(self):
+        return y
+
+class Wild(object):
+    def __init__(self, x, y):
+        self.y = y
+        self.x = x
+    def getX(self):
+        return self.x
+    def getY(self):
+        return self.y
+
+X = 7
+Y = 8
