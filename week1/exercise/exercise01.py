625805deb838cbaf67cc9984e34a4f5746b75679
Luiz Eduardo <29684170+menezesluiz@users.noreply.github.com>
2020-08-12 16:58:42 -0300

    MITx_edX

---
+++ week1/exercise/exercise01.py
@@ -0,0 +1,19 @@
+"""
+Exercise 1
+Finger Exercises due Aug 5, 2020 20:30 -03
+Completed
+
+Exercise 1
+7/7 points (graded)
+ESTIMATED TIME TO COMPLETE: 5 minutes
+Note that you will have to answer all questions before you can click the Check
+button.
+
+For each of the following expressions, indicate the value returned, or if the
+evaluation would lead to an error, write the word 'error' (note this is a word,
+not a string, no quotes). While you could simply type these expressions into
+your IDE, we encourage you to answer them directly since this will help
+reinforce your understanding of basic Python expressions.
+
+"""
+
