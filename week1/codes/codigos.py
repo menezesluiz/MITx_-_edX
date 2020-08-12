6f140df6718b8864eac18a245f655337166439de
Luiz Eduardo <29684170+menezesluiz@users.noreply.github.com>
2020-08-12 16:18:34 -0300

    MITx_edX

---
+++ week1/codes/codigos.py
@@ -0,0 +1,23 @@
+# Vídeo: Bindings
+
+x = 2
+x = x * x
+y = x + 1
+print(y)
+
+
+# Dessa forma, é mais fácil de se perder
+x = 1
+y = 2
+y = x
+x = y
+print(x)
+
+# A solução seria criar uma variável temporária
+
+x = 1
+y = 2
+temp = y
+y = x
+x = temp
+print(x)
