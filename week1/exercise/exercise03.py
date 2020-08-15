bdcb707f74dbf1b39f7dd110f05dd24b931c8aac
Luiz Eduardo <29684170+menezesluiz@users.noreply.github.com>
2020-08-14 23:27:46 -0300

    MITx_-_edX

---
+++ week1/exercise/exercise03.py
@@ -0,0 +1,60 @@
+"""
+Exercise 3
+Finger Exercises due Aug 5, 2020 20:30 -03
+Completed
+
+Exercise 3
+5/5 points (graded)
+ESTIMATED TIME TO COMPLETE: 5 minutes
+
+For each of the following expressions, indicate the value that prints out when 
+the expression is evaluated. If the evaluation would lead to an error, write 
+the word 'error'; if nothing would print out, write the word 'blank'.
+
+While you could simply type these expressions into your IDE, we encourage you 
+to answer them directly since this will help reinforce your understanding of 
+basic Python expressions.
+
+If the temperatures seem weird to you, like most of the world, you probably 
+use the Celsius system. We Americans still use the crazy Fahrenheit system
+"""
+# exercise 1
+if 6 > 7:
+    print("Yep")
+
+# exercise 2
+if 6 > 7:
+    print("Yep")
+else:
+    print("Nope")
+
+# exercise 3
+var = "Panda"
+if var == "panda":
+    print("Cute!")
+elif var == "Panda":
+    print("Regal!")
+else:
+    print("Ugly...")
+
+# exercise 4
+temp = 120
+if temp > 85:
+    print("Hot")
+elif temp > 100:
+    print("REALLY HOT!")
+elif temp > 60:
+    print("Confortable")
+else:
+    print("Cold")
+
+# exercise 5
+temp = 50
+if temp > 85:
+    print("Hot")
+elif temp > 100:
+    print("REALLY HOT!")
+elif temp > 60:
+    print("Confortable")
+else:
+    print("Cold")
