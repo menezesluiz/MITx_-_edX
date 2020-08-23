8a49895703412b8ce6f6382ea3cd9fde763d0f68
Luiz Eduard <29684170+menezesluiz@users.noreply.github.com>
2020-08-22 23:48:40 -0300

    MITx_-_edX

---
+++ week1/exercise/exercise-hello_world.py
@@ -0,0 +1,14 @@
+"""
+Exercise: hello world
+Finger Exercises due Aug 5, 2020 20:30 -03
+Completed
+
+Exercise: hello world
+5.0/5.0 points (graded)
+ESTIMATED TIME TO COMPLETE: 2 minutes
+
+Write a piece of Python code that prints out the string hello world
+
+"""
+# Neste exercício devo imprimir a frase "hello world"
+print("hello world")
--- week1/exercise/exercise04.py
+++ week1/exercise/exercise04.py
@@ -1,14 +1,53 @@
 """
-Exercise: hello world
+Exercise 4
 Finger Exercises due Aug 5, 2020 20:30 -03
 Completed
+ Bookmark this page
+Exercise 4
+5/5 points (graded)
+ESTIMATED TIME TO COMPLETE: 8 minutes
 
-Exercise: hello world
-5.0/5.0 points (graded)
-ESTIMATED TIME TO COMPLETE: 2 minutes
+Below are some short Python programs. For each program, answer the associated
+question.
 
-Write a piece of Python code that prints out the string hello world
+Try to answer the questions without running the code. Check your answers,
+then run the code for the ones you get wrong.
+
+This question is going to ask you what some simple loops print out. If you're
+asked what code like this prints:
+"""
+
+# Exemplo
+num = 5
+if num > 2:
+    print(num)
+    num -= 1
+print(num)
+
+# write what it prints out, separating what appears on a new line by a comma
+# and a space. So the answer for the above code would be:
+
+# Resposta: 5, 4
 
 """
-# Neste exercício devo imprimir a frase "hello world"
-print("hello world")
+If a given loop will not terminate, write the phrase 'infinite loop' 
+(no quotes) in the box. Recall that you can stop an infinite loop in your 
+program by typing CTRL+c in the console.
+
+Note: What does +=, -=, *=, /= stand for?
+a += b is equivalent to a = a + b
+
+a -= b is equivalent to a = a - b
+
+a *= b is equivalent to a = a * b
+
+a /= b is equivalent to a = a / b
+"""
+
+num = 0
+while num <= 5:
+    print(num)
+    num += 1
+
+print("Outside of loop")
+print(num)
