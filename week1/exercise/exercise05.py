ad9f841c211d2c0fe9d0b03a34421c262dd3981c
Luiz Eduard <29684170+menezesluiz@users.noreply.github.com>
2020-08-22 23:38:32 -0300

    MITx_-_edX

---
+++ week1/exercise/exercise05.py
@@ -0,0 +1,19 @@
+"""
+Exercise: happy
+Finger Exercises due Aug 5, 2020 20:30 -03
+Completed
+ Bookmark this page
+Exercise: happy
+5.0/5.0 points (graded)
+ESTIMATED TIME TO COMPLETE: 3 minutes
+
+Write a piece of Python code that prints out the string 'hello world' if the
+value of an integer variable, happy, is strictly greater than 2.
+
+Do not ask for user input
+Assume that variables are defined for you by our grader code. Simply write
+code assuming you know the values of the variables.
+"""
+happy = 5 # variável inexistente no exercício (desconsiderar)
+if happy > 2:
+    print('hello world')
