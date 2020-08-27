1f46a00a6aa3d4ef89194f64602f7d90941e6614
Luiz Eduard <29684170+menezesluiz@users.noreply.github.com>
2020-08-27 14:26:58 -0300

    MITx_-_edX

--- week1/exercise/exercise04.py
+++ week1/exercise/exercise04.py
@@ -44,6 +44,7 @@ a *= b is equivalent to a = a * b
 a /= b is equivalent to a = a / b
 """
 
+# 1
 num = 0
 while num <= 5:
     print(num)
@@ -51,3 +52,44 @@ while num <= 5:
 
 print("Outside of loop")
 print(num)
+# Minha resposta:
+# 0, 1, 2, 3, 4, 5, Outside of loop, 5 ou 6
+
+# 2
+numberOfLoops = 0
+numberOfApples = 2
+while numberOfLoops < 10:
+    numberOfApples *= 2
+    numberOfApples += numberOfLoops
+    numberOfLoops -= 1
+print("Number of apples: " + str(numberOfApples))
+# Minha resposta:
+# Infinite Loop
+
+
+# 3
+num = 10
+while True:
+    if num < 7:
+        print("Breaking out of loop")
+        break
+    print(num)
+    num -= 1
+print("Outside of loop")
+"""
+Note: If the command break is executed within a loop, it halts evaluation of 
+the loop at that point and passes control to the next expression. 
+Test some break statements inside different loops if you don't understand this 
+concept!
+"""
+# Minha resposta:
+# Breaking out of loop, 10, 9, Outside Of loop
+
+# 5
+num = 100
+while not False:
+    if num < 0:
+        break
+print('num is: ' + str(num))
+# Minha resposta:
+# Infinit loop
