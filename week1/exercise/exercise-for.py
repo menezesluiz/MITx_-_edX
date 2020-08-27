0d211bbd89a7b304fb78a7815f15d7880f74416c
Luiz Eduard <29684170+menezesluiz@users.noreply.github.com>
2020-08-27 14:37:49 -0300

    MITx_-_edX

---
+++ week1/exercise/exercise-for.py
@@ -0,0 +1,23 @@
+"""
+Exercise: for
+Finger Exercises due Aug 5, 2020 20:30 -03
+Completed
+ Bookmark this page
+Exercise: for exercise 1
+5.0/5.0 points (graded)
+ESTIMATED TIME TO COMPLETE: 5 minutes
+
+In this problem you'll be given a chance to practice writing some for loops.
+
+1. Convert the following code into code that uses a for loop.
+
+prints 2
+prints 4
+prints 6
+prints 8
+prints 10
+prints Goodbye!
+"""
+for i in range(2, 12, 2):
+    print(i)
+print("Goodbye!")
